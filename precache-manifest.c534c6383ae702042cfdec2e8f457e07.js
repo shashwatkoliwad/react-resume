ca1ddde170836a950caa688e0083d0ebf1872a7c
Shashwat Koliwad <skoliwad@cisco.com>
2019-11-25 06:38:37 +0900

    Updates

---
+++ precache-manifest.c534c6383ae702042cfdec2e8f457e07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9f141aa1cb0a75918bd12224aa7c3e",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "0021a7f789b24d349bf6",
+    "url": "/react-resume/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2d1ba2fccad745e18dfa",
+    "url": "/react-resume/static/js/2.2b64b0e6.chunk.js"
+  },
+  {
+    "revision": "0021a7f789b24d349bf6",
+    "url": "/react-resume/static/js/main.efae95d7.chunk.js"
+  },
+  {
+    "revision": "14c9837a3532269e597f",
+    "url": "/react-resume/static/js/runtime-main.d959a6bc.js"
+  }
+]);
